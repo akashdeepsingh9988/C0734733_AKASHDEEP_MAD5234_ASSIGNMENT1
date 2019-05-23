3528dc93dbd4d6eddfdc9c965eca7b1303523aa1
Akashdeep Singh <akashthind007@gmail.com>
2019-05-23 08:55:19 -0400

    requirements 1 - 4 added

--- C0734733_AKASHDEEP_MAD5234_ASSIGNMENT1/src/Yelling/Yelling.java
+++ C0734733_AKASHDEEP_MAD5234_ASSIGNMENT1/src/Yelling/Yelling.java
@@ -10,5 +10,22 @@ package Yelling;
  * @author Akashdeep Singh
  */
 public class Yelling {
-    
+
+    public String scream(String... name) {
+        boolean isUpperCase = Character.isUpperCase(name[0].charAt(0));
+        boolean lowercase2 = Character.isLowerCase(name[0].charAt(1));
+        if (isUpperCase == true && lowercase2 == true) {
+            return name[0] + "is yelling";
+        } else if (name[0].equals(name[0].toUpperCase())) {
+            return name[0] + "IS YELLING";
+        } else if (name[0] == null) {
+            return "Nobody is yelling";
+        } //for two values
+        else if (name[0] != null && name[1] != null) {
+            return name[0] + "and" + name[1] + "are yelling";
+
+        }
+        return name[0];
+
+    }
 }
