5c1411e02c738a056717e19845962cd5ff89bcdb
Akashdeep Singh <akashthind007@gmail.com>
2019-05-25 12:09:34 -0400

    FINAL CODE CHANGES AND TESTS

--- C0734733_AKASHDEEP_MAD5234_ASSIGNMENT1/test/Yelling/YellingTest.java
+++ C0734733_AKASHDEEP_MAD5234_ASSIGNMENT1/test/Yelling/YellingTest.java
@@ -17,6 +17,10 @@ import static org.junit.Assert.*;
  */
 public class YellingTest {
 
+    Yelling instance;
+    String result = "";
+    String expResult = "";
+
     public YellingTest() {
     }
 
@@ -30,6 +34,7 @@ public class YellingTest {
 
     @Before
     public void setUp() {
+        instance = new Yelling();
     }
 
     @After
@@ -39,18 +44,76 @@ public class YellingTest {
     /**
      * Test of scream method, of class Yelling.
      */
+   
+    
+    // ======================    METHOD 1 =========================================================
+    @Test
+    public void testScreamTC1() {
+        // TEST CASE 1 -  REQUIREMENT 1
+        result = instance.scream("Peter");
+        expResult = "Peter is yelling";
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testScreamTC2() {
+        // TEST CASE 2 - REQUIREMENT 2 
+        result = instance.scream("PETER");
+        expResult = "PETER IS YELLING";
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testScreamTC3() {
+        //TEST CASE 3 -  REQUIREMENT 3 
+        result = instance.scream(null);
+        expResult = "Nobody is yelling";
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testScreamTC4() {
+        //TEST CASE 4 - REQUIREMENT 4
+        result = instance.scream("Jenelle", "Kadeem");
+        expResult = "Jenelle and Kadeem are yelling";
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testScreamTC5() {
+        //TEST CASE 5 - REQUIREMENT 5
+        result = instance.scream("Jenelle", "Kadeem", "Akash");
+        expResult = "Jenelle,Kadeem and Akash are yelling";
+        assertEquals(expResult, result);
+
+    }
+
+    @Test
+    public void testScreamTC6() {
+        //TEST CASE 6 - REQUIREMENT 6
+        /*
+            four inputs with one CAPITAL name
+         */
+        result = instance.scream("AKASH", "Kadeem", "Akash", "Java");
+        expResult = "SO IS AKASH. Kadeem,Akash and Java is yelling";
+        assertEquals(expResult, result);
+    }
+
+    //=========================== METHOD 2 =====================================================
+   
+    /*
     @Test
     public void testScream() {
 
         System.out.println("scream");
-        String name = null;
         // TEST CASE 1 -  REQUIREMENT 1
-        Yelling instance = new Yelling();
-        String result = instance.scream("Peter");
-        String expResult = "Peter is yelling";
+        result = instance.scream("Peter");
+        expResult = "Peter is yelling";
         assertEquals(expResult, result);
-
-        // TEST CASE 2 - REQUIREMENT 2 
+        
+         // TEST CASE 2 - REQUIREMENT 2 
         result = instance.scream("PETER");
         expResult = "PETER IS YELLING";
         assertEquals(expResult, result);
@@ -71,13 +134,16 @@ public class YellingTest {
         assertEquals(expResult, result);
 
         //TEST CASE 6 - REQUIREMENT 6
-        /*
-            four inputs with one CAPITAL name
-        */
+        //
+          //  four inputs with one CAPITAL name
+         //
         result = instance.scream("AKASH", "Kadeem", "Akash", "Java");
         expResult = "SO IS AKASH. Kadeem,Akash and Java is yelling";
         assertEquals(expResult, result);
 
     }
+*/
 
+    
+    
 }
